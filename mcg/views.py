71c2a2fd1078f324b73b98df52919d7048364ff0
developers-wormos <user12himanshu@gmail.com>
2024-04-01 18:11:16 +0530

    remove only admin download for reports

--- mcg/views.py
+++ mcg/views.py
@@ -7,3 +7,15 @@ def privacy_policy(request):
 
 def terms_of_service(request):
     return render(request, 'terms.html')
+
+
+def return_policy(request):
+    return render(request, 'return.html')
+
+
+def refund_policy(request):
+    return render(request, 'refund.html')
+
+
+def shipping_policy(request):
+    return render(request, 'shipping.html')
